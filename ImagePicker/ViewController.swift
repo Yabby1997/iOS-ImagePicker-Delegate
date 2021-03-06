f91839ffb9f696abe94844524d2a20b2c8ae5d5d
seunghun yang <yabby1997@gmail.com>
2021-03-06 17:40:29 +0900

    Image picker를 통해 Image view의 이미지를 선택하도록 delegate를 사용

--- ImagePicker/ViewController.swift
+++ ImagePicker/ViewController.swift
@@ -7,13 +7,37 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
 
+    lazy var imagePicker: UIImagePickerController = {
+        let picker: UIImagePickerController = UIImagePickerController()
+        picker.sourceType = .photoLibrary
+        picker.delegate = self
+        return picker
+    }()
+    
+    @IBOutlet weak var imageView: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        self.dismiss(animated: true, completion: nil)
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        if let image = info[UIImagePickerController.InfoKey.originalImage] as? UIImage {
+            imageView.image = image
+        }
+        
+        self.dismiss(animated: true, completion: nil)
+    }
+        
+    @IBAction func touchUpSelectImageButton(_ sender: Any) {
+        self.present(self.imagePicker, animated: true, completion: nil)
+    }
+    
 }
 
